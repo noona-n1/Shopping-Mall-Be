cba19d9e6a3159c1dcbaeab87993ae119108af18
etesongg <etesong0114@gmail.com>
2024-11-12 22:56:58 +0900

    chore : be 초기 세팅 #1

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const indexRouter = require("./routers/index");
+const app = express();
+
+require("dotenv").config();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api", indexRouter);
+const mongoURI = process.env.MONGODB_URI_PROD;
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log("mongoose connected"))
+  .catch((err) => console.log("DB connection fail", err));
+
+app.listen(process.env.PORT || 5001, () => {
+  console.log("server on");
+});
