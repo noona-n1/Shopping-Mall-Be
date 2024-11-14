215f67602c76d4deac487700196893b11f14b75a
etesongg <etesong0114@gmail.com>
2024-11-14 20:09:49 +0900

    feat : create getAddress controller #17

--- controllers/addressList.controller.js
+++ controllers/addressList.controller.js
@@ -14,4 +14,20 @@ addressListController.addAddress = async (req, res) => {
   }
 };
 
+addressListController.getAddress = async (req, res) => {
+  try {
+    const { userId } = req;
+    const addresses = await AddressList.find({ userId });
+    if (addresses.length > 0) {
+      return res.status(200).json({ status: "success", data: addresses });
+    } else {
+      return res
+        .status(200)
+        .json({ status: "success", message: "저장된 주소가 없습니다." });
+    }
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = addressListController;
--- routers/addressList.api.js
+++ routers/addressList.api.js
@@ -5,5 +5,6 @@ const authController = require("../controllers/auth.controller");
 const addressListController = require("../controllers/addressList.controller");
 
 router.post("/", authController.authenticate, addressListController.addAddress);
+router.get("/", authController.authenticate, addressListController.getAddress);
 
 module.exports = router;
