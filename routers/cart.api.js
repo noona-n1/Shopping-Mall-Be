5cc61838d5a9515c41e47474160108a992f641c2
jean9710 <jean9710@proten.co.kr>
2024-11-19 09:00:41 +0900

    카트 비회원도 추가,삭제,조회 가능 원복 #48

--- routers/cart.api.js
+++ routers/cart.api.js
@@ -4,22 +4,27 @@ const cartController = require("../controllers/cart.controller");
 const authController = require("../controllers/auth.controller");
 
 router.post("/", 
+    authController.authenticate, 
     cartController.createCart
 );
 
 router.get("/", 
+    authController.authenticate, 
     cartController.getCart
 );
 
 router.get("/count", 
+    authController.authenticate, 
     cartController.getCartCount
 );
 
 router.delete("/:itemId", 
+    authController.authenticate, 
     cartController.deleteCartItem
 );
 
 router.put("/:itemId", 
+    authController.authenticate, 
     cartController.updateCartItem
 );
 
