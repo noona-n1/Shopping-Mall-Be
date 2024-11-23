e6617d5c531137905bc6633c02b83d4c9c6e0f97
Doni <48715999+gganj123@users.noreply.github.com>
2024-11-23 19:12:51 +0900

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -38,4 +38,20 @@ userController.getUser = async (req, res) => {
   }
 };
 
+userController.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ status: 'fail', message: 'User not found' });
+    }
+
+    res.status(200).json({ status: 'success', message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ status: 'fail', message: error.message });
+  }
+};
+
+
 module.exports = userController;
